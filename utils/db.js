9e0b16d684b0a2235eb8073852043b8c4ee0b792
Azeez Adebayo <debaycisse@gmail.com>
2024-09-29 11:15:59 +0100

    implement task 1. MongoDB utils
    script implement DBClient class, which establishes connection to a mongoDB, with methods to check for the db connectivity, and a method to obtain a number of a particular document in a collection

---
+++ utils/db.js
@@ -0,0 +1,58 @@
+const { MongoClient } = require('mongodb');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+class DBClient {
+  constructor() {
+    this.mongoClient = new MongoClient(URI, { useUnifiedTopology: true });
+    this.isConnected = false;
+
+    this.mongoClient.connect()
+      .then(() => {
+        this.isConnected = true;
+      })
+      .catch(() => {
+        this.isConnected = false;
+      });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    if (!this.isConnected) {
+      return null;
+    }
+
+    try {
+      const dbName = this.mongoClient.db();
+      const usersCollection = dbName.collection('users');
+      const numOfUsers = usersCollection.countDocuments({});
+      return numOfUsers;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async nbFiles() {
+    if (!this.isConnected) {
+      return null;
+    }
+
+    try {
+      const dbName = this.mongoClient.db();
+      const filesCollection = dbName.collection('files');
+      const numOfFiles = filesCollection.countDocuments({});
+      return numOfFiles;
+    } catch (error) {
+      return null;
+    }
+  }
+}
+const dbClient = new DBClient();
+
+module.exports = dbClient;
